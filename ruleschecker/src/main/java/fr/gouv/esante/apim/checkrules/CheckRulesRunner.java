fd1c1e1968d5d0685ea523e3bd8ae5034fdd6e5f
lpoma <lpoma@nx-domain.lan>
2024-09-11 14:41:02 +0200

    Revise project structure to inherit from reactor project instead of Spring parent

--- ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/CheckRulesRunner.java
+++ ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/CheckRulesRunner.java
@@ -9,19 +9,21 @@ import fr.gouv.esante.apim.client.model.ApiGravitee;
 import fr.gouv.esante.apim.client.model.ApisResponseGravitee;
 import fr.gouv.esante.apim.client.model.PlanGravitee;
 import fr.gouv.esante.apim.client.model.PlansResponseGravitee;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Component
 @Slf4j
+@Profile({ "production", "test" })
 public class CheckRulesRunner implements CommandLineRunner {
-
-    String defaultMessage = "Implémenter les contrôles";
 
     private static final String ENV_ID = "environment identfier";
 
@@ -39,35 +41,38 @@ public class CheckRulesRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        log.info(defaultMessage);
+        log.info("Starting check rules");
         // get data from args
         // run checks
         check();
-        log.info(defaultMessage);
+        log.info("Finished check rules");
     }
 
     private void check() {
-        // Initialiser la liste de résultats
+        // Initialize results list
         Map<String, ApiDefinitionCheckResult> allResults = new HashMap<>();
 
         try {
-            // Appel de chaque API definition
+            // Get API Definitions list
             ApisResponseGravitee apisResponse = apisApi.listApis(
                     ENV_ID,
                     1,
                     100,
-                    null
+                    Collections.emptyList()
             );
             List<ApiGravitee> apis = apisResponse.getData();
 
-            // Récupération des plans de chaque API
+            // Test each API
             for (ApiGravitee apiGravitee : apis) {
+                // Get each individual API Definition
                 ApiGravitee apiDefinition = apisApi.getApi(ENV_ID, apiGravitee.getId());
+
+                // Get plans list belonging to the API
                 PlansResponseGravitee plansResponse = apiPlansApi.listApiPlans(
                         ENV_ID,
                         apiGravitee.getId(),
-                        null,
-                        null,
+                        Collections.emptyList(),
+                        Collections.emptyList(),
                         null,
                         1,
                         100
@@ -75,9 +80,9 @@ public class CheckRulesRunner implements CommandLineRunner {
                 List<PlanGravitee> plans = plansResponse.getData();
                 List<PlanGravitee> fullPlans = plansResponse.getData();
 
-                // (Vérifier si cette étape est vraiment nécessaire)
+                // (Confirm with real API response if this step is really needed)
                 for (PlanGravitee planDetails : plans) {
-                    // Get (again) API plans from their ids and store in 2nd list
+                    // Get (again) API plans from their ids and store it in fullPlans list
                     fullPlans.add(
                             apiPlansApi.getApiPlan(ENV_ID, apiGravitee.getId(), planDetails.getId())
                     );
@@ -86,7 +91,7 @@ public class CheckRulesRunner implements CommandLineRunner {
                 // Build an object GraviteeApiDefinition
                 GraviteeApiDefinition graviteeApiDefinition = new GraviteeApiDefinition(apiDefinition, fullPlans);
 
-                // Pour chaque API on lance RuleChecker
+                // Execute RuleChecker for each API and collect results
                 allResults.put(apiDefinition.getName(), rulesChecker.checkRules(graviteeApiDefinition));
             }
         } catch (Exception e) {
@@ -95,7 +100,7 @@ public class CheckRulesRunner implements CommandLineRunner {
             log.error("END ERROR");
         }
 
-        // Collecter résultats et lancer les notifs
+        // Send notifications
         log.info(allResults.toString());
 
     }
--- ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/config/AppConfig.java
+++ ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApiConfig {
+public class AppConfig {
 
     @Bean
     public ApisApi apisApi() {
