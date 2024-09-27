836fc9e96e1bda52a8981baf3941bcdc0cacdbc2
LionelPomaANS <lionel.poma.ext@esante.gouv.fr>
2024-09-27 16:47:25 +0200

    Implémente le contrôle de la règle 3.1.1 - authentification et identification de l'utilisateur

---
+++ ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/rules/impl/SecuredPlan.java
@@ -0,0 +1,51 @@
+package fr.gouv.esante.apim.checkrules.rules.impl;
+
+import fr.gouv.esante.apim.checkrules.model.GraviteeApiDefinition;
+import fr.gouv.esante.apim.checkrules.model.RuleResult;
+import fr.gouv.esante.apim.checkrules.rules.ApiDefinitionQualityRule;
+import fr.gouv.esante.apim.client.model.PlanEntityGravitee;
+import fr.gouv.esante.apim.client.model.PlanSecurityTypeGravitee;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+
+@Slf4j
+public class SecuredPlan implements ApiDefinitionQualityRule {
+
+    protected static final String FAILURE_MSG = "Aucun plan ne dispose d'un moyen d’authentification et " +
+            "d’identification de l’utilisateur sur cette API";
+    protected static final String SUCCESS_MSG = "Plan sécurisé présent sur cette API";
+
+
+    @Override
+    public String getName() {
+        return "3.1.1 - Paramétrer au moins un moyen d’authentification et d’identification de l’utilisateur";
+    }
+
+    @Override
+    public RuleResult visit(GraviteeApiDefinition apiDefinition) {
+        log.info("SecuredPlan visit");
+        Set<PlanEntityGravitee> plans = apiDefinition.getPlans();
+        boolean success = verify(plans);
+        return new RuleResult(
+                getName(),
+                success,
+                success ? SUCCESS_MSG : FAILURE_MSG
+        );
+    }
+
+    private boolean verify(Set<PlanEntityGravitee> plans) {
+        boolean success = false;
+        if (plans == null || plans.isEmpty()) {
+            return false;
+        }
+        for (PlanEntityGravitee plan : plans) {
+            if (plan.getSecurity() == PlanSecurityTypeGravitee.API_KEY
+                    || plan.getSecurity() == PlanSecurityTypeGravitee.OAUTH2) {
+                success = true;
+                break;
+            }
+        }
+        return success;
+    }
+}
---
+++ ruleschecker/src/test/java/fr/gouv/esante/apim/checkrules/rules/impl/SecuredPlanTest.java
@@ -0,0 +1,101 @@
+package fr.gouv.esante.apim.checkrules.rules.impl;
+
+import fr.gouv.esante.apim.checkrules.model.GraviteeApiDefinition;
+import fr.gouv.esante.apim.checkrules.model.RuleResult;
+import fr.gouv.esante.apim.checkrules.services.ApiDefinitionMapper;
+import fr.gouv.esante.apim.client.model.PlanEntityGravitee;
+import fr.gouv.esante.apim.client.model.PlanSecurityTypeGravitee;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = {SecuredPlanTest.class, ApiDefinitionMapper.class})
+@ActiveProfiles({ "test" })
+@Slf4j
+class SecuredPlanTest extends SecuredPlan {
+
+    @Test
+    void testSecuredPlanExists() {
+        GraviteeApiDefinition apiDef = new GraviteeApiDefinition();
+        Set<PlanEntityGravitee> plans = new HashSet<>();
+        PlanEntityGravitee plan = new PlanEntityGravitee();
+        plan.setSecurity(PlanSecurityTypeGravitee.API_KEY);
+        plans.add(plan);
+        apiDef.setPlans(plans);
+        SecuredPlan securedPlanRule = new SecuredPlan();
+        RuleResult result = apiDef.accept(securedPlanRule);
+
+        assertEquals(super.getName(), result.getRuleName());
+        assertTrue(result.isSuccess());
+        assertEquals(SUCCESS_MSG, result.getMessage());
+    }
+
+    @Test
+    void testNoPlanExists() {
+        GraviteeApiDefinition apiDef = new GraviteeApiDefinition();
+        SecuredPlan securedPlanRule = new SecuredPlan();
+        RuleResult result = apiDef.accept(securedPlanRule);
+
+        assertFalse(result.isSuccess());
+        assertEquals(FAILURE_MSG, result.getMessage());
+    }
+
+    @Test
+    void testSecuredPlanDoesNotExist() {
+        GraviteeApiDefinition apiDef = new GraviteeApiDefinition();
+        Set<PlanEntityGravitee> plans = new HashSet<>();
+        PlanEntityGravitee keyLessPlan = new PlanEntityGravitee();
+        keyLessPlan.setSecurity(PlanSecurityTypeGravitee.KEY_LESS);
+        plans.add(keyLessPlan);
+        PlanEntityGravitee jwtPlan = new PlanEntityGravitee();
+        jwtPlan.setSecurity(PlanSecurityTypeGravitee.JWT);
+        plans.add(jwtPlan);
+        PlanEntityGravitee subscriptionPlan = new PlanEntityGravitee();
+        subscriptionPlan.setSecurity(PlanSecurityTypeGravitee.SUBSCRIPTION);
+        plans.add(subscriptionPlan);
+        PlanEntityGravitee noSecurityPlan = new PlanEntityGravitee();
+        plans.add(noSecurityPlan);
+        apiDef.setPlans(plans);
+        SecuredPlan securedPlanRule = new SecuredPlan();
+        RuleResult result = apiDef.accept(securedPlanRule);
+
+        assertFalse(result.isSuccess());
+    }
+
+    @Test
+    void testMultipleSecuredPlanExists() {
+        GraviteeApiDefinition apiDef = new GraviteeApiDefinition();
+        Set<PlanEntityGravitee> plans = new HashSet<>();
+        PlanEntityGravitee noSecurityPlan = new PlanEntityGravitee();
+        plans.add(noSecurityPlan);
+        PlanEntityGravitee apiKeyPlan = new PlanEntityGravitee();
+        apiKeyPlan.setSecurity(PlanSecurityTypeGravitee.API_KEY);
+        plans.add(apiKeyPlan);
+        PlanEntityGravitee oAuth2Plan = new PlanEntityGravitee();
+        oAuth2Plan.setSecurity(PlanSecurityTypeGravitee.OAUTH2);
+        plans.add(oAuth2Plan);
+        PlanEntityGravitee keyLessPlan = new PlanEntityGravitee();
+        keyLessPlan.setSecurity(PlanSecurityTypeGravitee.KEY_LESS);
+        plans.add(keyLessPlan);
+        apiDef.setPlans(plans);
+        SecuredPlan securedPlanRule = new SecuredPlan();
+        RuleResult result = apiDef.accept(securedPlanRule);
+
+        assertTrue(result.isSuccess());
+    }
+
+
+
+
+
+
+
+
+}
