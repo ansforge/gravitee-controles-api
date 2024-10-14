f174317e6aaa07b560be51890590f950896ff688
LionelPomaANS <lionel.poma.ext@esante.gouv.fr>
2024-10-14 13:53:49 +0200

    Surcharge de la méthode equals du modèle de Configuration

--- ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/model/Configuration.java
+++ ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/model/Configuration.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -15,4 +16,17 @@ public class Configuration {
 
     private List<Filter> whitelist;
     private List<Filter> blacklist;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Configuration that = (Configuration) o;
+        return Objects.equals(whitelist, that.whitelist) && Objects.equals(blacklist, that.blacklist);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(whitelist, blacklist);
+    }
 }
