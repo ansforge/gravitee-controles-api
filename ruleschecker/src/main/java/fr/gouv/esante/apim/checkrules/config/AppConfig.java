3249fe83d1ee1700d492c1ba50fe0efef44558eb
LionelPomaANS <lionel.poma.ext@esante.gouv.fr>
2024-10-18 11:35:49 +0200

    Supprime l'auth au serveur SMTP

--- ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/config/AppConfig.java
+++ ruleschecker/src/main/java/fr/gouv/esante/apim/checkrules/config/AppConfig.java
@@ -67,8 +67,6 @@ public class AppConfig {
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
